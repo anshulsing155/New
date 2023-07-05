95b7c9ae6cbff498ada3d10e246517eac34b70af
Anshul Singh <anshulsing155>
2023-07-06 00:31:41 +0530

    property location page update

--- rinn/src/routes/start/Property-Location/+page.svelte
+++ rinn/src/routes/start/Property-Location/+page.svelte
@@ -2,23 +2,62 @@
     import { goto } from "$app/navigation";
     import ProgressBar from "../../components/ProgressBar.svelte";
     let progress = 21.45;
+    import data from "$lib/Indian-States-&-Cities.json";
 
     function onChange(event) {
-        var inputElement = document.getElementById("myInput1");
+        var inputElement = document.getElementById("state");
         var inputValue1 = inputElement.value;
-        var inputElement = document.getElementById("myInput2");
+        var inputElement = document.getElementById("city");
         var inputValue2 = inputElement.value;
-        document.cookie = "Property Location (State)="+inputValue1;
-        document.cookie = "Property Location (City)="+inputValue2;
+        document.cookie = "Property Location (State)=" + inputValue1;
+        document.cookie = "Property Location (City)=" + inputValue2;
         if (event.currentTarget.value == "next") {
             goto("../start/Working-Location");
         }
-        
+    }
+    let state = [];
+    for (const key in data) {
+        state.push(key);
+    }
+    let newState = state.sort();
+
+    
+
+
+    // let newCity = city.sort();
+
+    // City data based on selected state
+    // const cityData = {
+    //     California: ["Los Angeles", "San Francisco", "San Diego"],
+    //     "New York": ["New York City", "Buffalo", "Rochester"],
+    //     Texas: ["Houston", "Austin", "Dallas"],
+    // };
+
+    // Function to populate city options based on selected state
+    function populateCities() {
+        const stateSelect = document.getElementById("state");
+        const citySelect = document.getElementById("city");
+        const selectedState = stateSelect.value;
+
+        // Clear previous city options
+        citySelect.innerHTML = "<option value=''>-- Select a City --</option>";
+
+        if (selectedState !== "") {
+            let city = data[selectedState];
+            const cities = city.sort();
+            for (let i = 0; i < cities.length; i++) {
+                const option = document.createElement("option");
+                option.value = cities[i];
+                option.text = cities[i];
+                citySelect.appendChild(option);
+            }
+        }
     }
 </script>
 
+<!-- {data1} -->
 <ProgressBar {progress} />
-<main>
+<!-- <main>
     <form class="form-container">
         <label for=""
             >Property State:
@@ -39,77 +78,33 @@
         <label class="next">
             <input on:change={onChange} type="radio" value="next" />Next
         </label>
-        <!-- <button on:change={onChange} value="next">Next</button> -->
-        <!-- <button type="submit">Submit</button> -->
     </form>
-</main>
-
+</main> -->
+<section class="w-1/3 m-auto mb-10">
+    <label
+    for="state"
+        class=" block mb-4 text-center text-xl font-medium text-gray-900 dark:text-white"
+        >Select Your Property Location</label
+    >
+    <label for="state">Select State:</label>
+    <select
+        id="state" on:change={populateCities}
+        class="mb-8 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+    >
+        <option>Select Property State</option>
+        {#each newState as item}
+            <option value={item}>{item}</option>
+        {/each}
+    </select>
+    <label for="city">Select City:</label>
+    <select
+        id="city"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+    >
+        <option value="">Select Property City</option>
+    </select>
+   <button on:click={onChange} class="my-8 px-10 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="next">Next</button>
+</section>
 <style>
-    @import url("https://fonts.googleapis.com/css?family=Muli&display=swap");
- 
- * {
-     box-sizing: border-box;
- }
- 
- main{
-     font-family: "Muli", sans-serif;
-     display: flex;
-     align-items: center;
-     justify-content: center;
     
-     overflow: hidden;
-     margin: 0;
- }.form-container {
-     background-color: #ffffff;
-     /* border-radius: 10px; */
-     /* box-shadow: 0 10px 20px rgba(0, 0, 0, 0.1), 0 6px 6px rgba(0, 0, 0, 0.1); */
-     padding-bottom: 100px;
-     text-align: center;
-     max-width: 100%;
-     width: 60%;
- }
- label {
-     display: block;
-     text-align: left;
-     border: 1;
-     padding: 1.5rem;
- 
-     box-shadow: 0 10px 20px rgba(0, 0, 0, 0.1), 0 6px 6px rgba(0, 0, 0, 0.1);
-     border-radius: 10px;
-     width: 40%;
-     margin: 10px auto;
- 
- }
- input[type="text"]:hover {
-     display: block;
-     border: 1;
-
-     border: 2px solid rgb(15, 61, 15);
-     border-radius: 10px;
- 
- }
-     input[type="text"] {
-        padding: 0.5rem 0;
-        margin-top: 0.5rem;
-        border-width: 1px;
-        margin: 1rem 0;
-        text-align: left;
-        border: 1;
-        padding: 1em;
-        border: 1px solid gray;
-        border-radius: 10px;
-    }
-    input[type="radio"] {
-        visibility: hidden;
-    }
-
-    .next {
-       margin-top: 5%;
-        text-align: center;
-        background-color: rgb(155, 155, 224);
-    }
-    /* button{
-        width: 100%;
-        padding: 10px;
-    } */
 </style>
