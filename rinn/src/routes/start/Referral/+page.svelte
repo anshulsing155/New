758ca219a43b2018aad8070d61165212c00f861a
Anshul Singh <anshulsing155>
2023-07-07 13:46:36 +0530

    path added

--- rinn/src/routes/start/Referral/+page.svelte
+++ rinn/src/routes/start/Referral/+page.svelte
@@ -4,9 +4,9 @@
     let progress = 71.5;
 
     function onChange(event) {
-        var inputElement = document.getElementById("myInput1");
+        var inputElement = document.getElementById("inline-full-name");
         var inputValue1 = inputElement.value;
-        var inputElement = document.getElementById("myInput2");
+        var inputElement = document.getElementById("inline-Contact");
         var inputValue2 = inputElement.value;
         document.cookie = "Referral Code (Name)=" + inputValue1;
         document.cookie = "Referral Code(Mobile No.)=" + inputValue2;
@@ -57,12 +57,12 @@
     </div>
     <div class="md:flex md:items-center mb-6 ">
       <div class="md:w-1/3">
-        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
+        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-Contact">
           Contact No.
         </label>
       </div>
       <div class="md:w-2/3 ">
-        <input class="bg-gray-200 appearance-none border-b-2 border-green-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500" id="inline-password" type="tel"  placeholder="123-45-678">
+        <input class="bg-gray-200 appearance-none border-b-2 border-green-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500" id="inline-Contact" type="tel"  placeholder="123-45-678">
       </div>
     </div>
     <!-- <div class="md:flex md:items-center mb-6">
@@ -77,7 +77,7 @@
     <div class="md:flex md:items-center">
       <div class="md:w-1/3 "></div>
       <div class="md:w-2/3 ">
-        <button class="shadow  bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button" value="next">
+        <button on:change={onChange} class="shadow  bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button" value="next">
         Next
         </button>
       </div>
