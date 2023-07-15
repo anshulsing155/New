e427cf89f96aaeb92783b195b768ae01e0fd7369
Anshul Singh <anshulsing155>
2023-07-15 17:21:39 +0530

    validation update

--- rinn/src/routes/start/Referral/+page.svelte
+++ rinn/src/routes/start/Referral/+page.svelte
@@ -1,93 +1,90 @@
 <script>
-    import { goto } from "$app/navigation";
-    import ProgressBar from "../../components/ProgressBar.svelte";
-    let progress = 71.5;
+  import { goto } from "$app/navigation";
+  import ProgressBar from "../../components/ProgressBar.svelte";
+  let progress = 71.5;
 
-    function onChange(event) {
-        var inputElement = document.getElementById("inline-full-name");
-        var inputValue1 = inputElement.value;
-        var inputElement = document.getElementById("inline-Contact");
-        var inputValue2 = inputElement.value;
-        document.cookie = "Referral Code (Name)=" + inputValue1;
-        document.cookie = "Referral Code(Mobile No.)=" + inputValue2;
-        if (event.currentTarget.value == "next") {
-            goto("../start/details");
-        }
+  function onChange(event) {
+    var inputElement = document.getElementById("inline-full-name");
+    var inputValue1 = inputElement.value;
+    var inputElement = document.getElementById("inline-Contact");
+    var inputValue2 = inputElement.value;
+    document.cookie = "Referral Code (Name)=" + inputValue1;
+    document.cookie = "Referral Code(Mobile No.)=" + inputValue2;
+    if (event.currentTarget.value == "next") {
+      goto("../start/details");
     }
+  }
+  let number = "";
+  let isValidMobileNumber = true;
+  function handleInput(event) {
+    number = event.target.value.replace(/\D/g, "");
+    number = number.slice(0, 10);
+    isValidMobileNumber = number.length === 10;
+  }
 </script>
 
 <ProgressBar {progress} />
-<!-- <main>
-    <form class="form-container">
-        <label for=""
-            >Name:
-            <input id="myInput1" type="Text" placeholder="Enter Name" />
-        </label>
-        <label for=""
-            >Contact No.:
-
-            <input type="text" id="myInput2" placeholder=" Enter Contact No." />
-        </label>
-        <label class="next">
-            <input
-                on:change={onChange}
-                type="radio"
-                name="amount"
-                value="next"
-            />Next
-        </label>
-    </form>
-</main> -->
-<section class="w-full max-w-md m-auto md:border mb-20 p-10 ">
-    <label
+<section class="w-full max-w-md m-auto md:border mb-20 p-10">
+  <label
     for="state"
-        class="  block mb-8 text-center text-xl font-medium text-gray-900"
-        >Person who referred you... </label
-    >
-    <div class="md:flex md:items-center mb-6">
-        
-      <div class="md:w-1/3 ">
-        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
-          Full Name
-        </label>
-      </div>
-      <div class="md:w-2/3 ">
-        <input class="bg-gray-200 appearance-none border-b-2 border-green-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500" id="inline-full-name" type="text" placeholder="Referral Name">
-      </div>
-    </div>
-    <div class="md:flex md:items-center mb-6 ">
-      <div class="md:w-1/3">
-        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-Contact">
-          Contact No.
-        </label>
-      </div>
-      <div class="md:w-2/3 ">
-        <input class="bg-gray-200 appearance-none border-b-2 border-green-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500" id="inline-Contact" type="tel"  placeholder="123-45-678">
-      </div>
-    </div>
-    <!-- <div class="md:flex md:items-center mb-6">
-      <div class="md:w-1/3"></div>
-      <label class="md:w-2/3 block text-gray-500 font-bold">
-        <input class="mr-2 leading-tight" type="checkbox">
-        <span class="text-sm">
-          Send me your newsletter!
-        </span>
+    class="  block mb-8 text-center text-xl font-medium text-gray-900"
+    >Person who referred you...
+  </label>
+  <div class="md:flex md:items-center mb-6">
+    <div class="md:w-1/3">
+      <label
+        class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+        for="inline-full-name"
+      >
+        Full Name
       </label>
-    </div> -->
-    <div class="md:flex md:items-center">
-      <div class="md:w-1/3 "></div>
-      <div class="md:w-2/3 ">
-        <label class="shadow  bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
-            <input
-                on:change={onChange}
-                type="radio"
-                class="hidden"
-                value="next"
-            />Next
-        </label>
-        <!-- <button on:change={onChange} class="shadow  bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button" value="next">
-        Next
-        </button> -->
-      </div>
     </div>
+    <div class="md:w-2/3">
+      <input
+        class="bg-gray-200 appearance-none border-b-2 border-green-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500"
+        id="inline-full-name"
+        type="text"
+        placeholder="Referral Name"
+      />
+    </div>
+  </div>
+  <div class="md:flex md:items-center mb-6">
+    <div class="md:w-1/3">
+      <label
+        class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+        for="inline-Contact"
+      >
+        Contact No.
+      </label>
+    </div>
+    <div class="md:w-2/3">
+      <input
+        class="bg-gray-200 appearance-none border-b-2 border-green-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500"
+        id="inline-Contact"
+        type="tel"
+        bind:value={number}
+        on:input={handleInput}
+        placeholder="123-45-678"
+      />
+      {#if !isValidMobileNumber}
+        <p class="text-red-300">Please enter a valid 10-digit mobile number</p>
+      {/if}
+    </div>
+  </div>
+
+  <div class="md:flex md:items-center">
+    <div class="md:w-1/3" />
+    <div class="md:w-2/3">
+      <label
+        class="shadow bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+      >
+        <input
+          on:change={onChange}
+          type="radio"
+          class="hidden"
+          value="next"
+        />Next
+      </label>
+    </div>
+  </div>
 </section>
--- rinn/src/routes/start/details/+page.svelte
+++ rinn/src/routes/start/details/+page.svelte
@@ -1,61 +1,121 @@
 <script>
-    import { goto } from "$app/navigation";
-    import ProgressBar from "../../components/ProgressBar.svelte";
-    let progress = 78.65;
-    function onChange(event) {
-        var inputElement = document.getElementById("inline-full-name");
-        var inputValue1 = inputElement.value;
-        var inputElement = document.getElementById("inline-Contact");
-        var inputValue2 = inputElement.value;
-        var inputElement = document.getElementById("inline-email");
-        var inputValue3 = inputElement.value;
-        document.cookie = "User (Name)=" + inputValue1;
-        document.cookie = "User (Mobile No.)=" + inputValue2;
-        document.cookie = "User (Email)=" + inputValue3;
-        if (event.currentTarget.value == "next") {
-            goto("../start/Documents");
-        }
+  import { goto } from "$app/navigation";
+  import ProgressBar from "../../components/ProgressBar.svelte";
+  let progress = 78.65;
+  function onChange(event) {
+    var inputElement = document.getElementById("inline-full-name");
+    var inputValue1 = inputElement.value;
+    var inputElement = document.getElementById("inline-Contact");
+    var inputValue2 = inputElement.value;
+    var inputElement = document.getElementById("inline-email");
+    var inputValue3 = inputElement.value;
+    document.cookie = "User (Name)=" + inputValue1;
+    document.cookie = "User (Mobile No.)=" + inputValue2;
+    document.cookie = "User (Email)=" + inputValue3;
+
+    if (event.currentTarget.value == "next") {
+      goto("../start/Documents");
     }
+  }
+  let name = "";
+  let mobileNumber = "";
+  let email = "";
+  let isNameValid = false;
+  let isMobileNumberValid = true;
+  let isEmailValid = false;
+  function validateName() {
+    // Validate the name field
+    isNameValid = name.trim() !== "";
+  }
+
+  function validateMobileNumber() {
+    // Remove any non-numeric characters from the input
+    mobileNumber = mobileNumber.replace(/\D/g, "");
+
+    // Limit the input to a maximum of 10 digits
+    mobileNumber = mobileNumber.slice(0, 10);
+
+    // Validate the mobile number field
+    isMobileNumberValid = mobileNumber.length === 10;
+  }
+
+  function validateEmail() {
+    // Validate the email field using a simple regular expression pattern
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    isEmailValid = emailPattern.test(email.trim());
+  }
 </script>
+
 <ProgressBar {progress} />
-<section class="w-full max-w-md m-auto md:border mb-20 p-10 ">
-    <label
+<section class="w-full max-w-md m-auto md:border mb-20 p-10">
+  <label
     for="state"
-        class="  block mb-8 text-center text-xl font-medium text-gray-900"
-        >Person who referred you... </label
-    >
-    <div class="md:flex md:items-center mb-6">
-        
-      <div class="md:w-1/3 ">
-        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-full-name">
-          Full Name :
-        </label>
-      </div>
-      <div class="md:w-2/3 ">
-        <input class="bg-gray-200 appearance-none border-b-2 border-green-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500" id="inline-full-name" type="text" placeholder="Enter Your Name">
-      </div>
+    class="  block mb-8 text-center text-xl font-medium text-gray-900"
+    >Your Details...!</label
+  >
+  <div class="md:flex md:items-center mb-6">
+    <div class="md:w-1/3">
+      <label
+        class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+        for="inline-full-name"
+      >
+        Full Name :
+      </label>
     </div>
-    <div class="md:flex md:items-center mb-6 ">
-      <div class="md:w-1/3">
-        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-Contact">
-          Contact No. :
-        </label>
-      </div>
-      <div class="md:w-2/3 ">
-        <input class="bg-gray-200 appearance-none border-b-2 border-green-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500" id="inline-Contact" type="tel"  placeholder="123-45-678">
-      </div>
+    <div class="md:w-2/3">
+      <input
+        class="bg-gray-200 appearance-none border-b-2 border-green-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500"
+        id="inline-full-name"
+        type="text"
+        bind:value={name}
+        on:blur={validateName}
+        placeholder="Enter Your Name"
+      />
     </div>
-    <div class="md:flex md:items-center mb-6 ">
-      <div class="md:w-1/3">
-        <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-email">
-          Email ID :
-        </label>
-      </div>
-      <div class="md:w-2/3 ">
-        <input class="bg-gray-200 appearance-none border-b-2 border-green-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500" id="inline-email" type="tel"  placeholder="abc@gmail.com">
-      </div>
+  </div>
+  <div class="md:flex md:items-center mb-6">
+    <div class="md:w-1/3">
+      <label
+        class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+        for="inline-Contact"
+      >
+        Contact No. :
+      </label>
     </div>
-    <!-- <div class="md:flex md:items-center mb-6">
+    <div class="md:w-2/3">
+      <input
+        class="bg-gray-200 appearance-none border-b-2 border-green-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500"
+        id="inline-Contact"
+        type="text"
+        bind:value={mobileNumber}
+        on:input={validateMobileNumber}
+        placeholder="123-45-678"
+      />
+    </div>
+  </div>
+  {#if !isMobileNumberValid}
+    <p class="text-red-300 mb-5">Please enter a valid 10-digit mobile number</p>
+  {/if}
+  <div class="md:flex md:items-center mb-6">
+    <div class="md:w-1/3">
+      <label
+        class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+        for="inline-email"
+      >
+        Email ID :
+      </label>
+    </div>
+    <div class="md:w-2/3">
+      <input
+        class="bg-gray-200 appearance-none border-b-2 border-green-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-green-500"
+        id="inline-email"
+        type="tel"
+        placeholder="abc@gmail.com"
+        required
+      />
+    </div>
+  </div>
+  <!-- <div class="md:flex md:items-center mb-6">
       <div class="md:w-1/3"></div>
       <label class="md:w-2/3 block text-gray-500 font-bold">
         <input class="mr-2 leading-tight" type="checkbox">
@@ -64,59 +124,27 @@
         </span>
       </label>
     </div> -->
-    <div class="md:flex md:items-center">
-      <div class="md:w-1/3 "></div>
-      <div class="md:w-2/3 ">
-        <label class="shadow  bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
-            <input
-                on:change={onChange}
-                type="radio"
-                class="hidden"
-                value="next"
-            />Next
-        </label>
-        <!-- <button on:change={onChange} class="shadow  bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button" value="next">
-        Next
-        </button> -->
-      </div>
+  <div class="md:flex md:items-center">
+    <div class="md:w-1/3" />
+    <div class="md:w-2/3">
+      <label
+        class="shadow bg-green-500 hover:bg-green-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+      >
+        <input
+          on:change={onChange}
+          disabled={!isNameValid || !isMobileNumberValid || !isEmailValid}
+          type="radio"
+          class="hidden"
+          value="next"
+        />Next
+      </label>
     </div>
+  </div>
 </section>
-<!-- <main>
-    <form class="form-container">
-        <label for=""
-            >Name:
-            <input
-            id="myInput1"
-                type="Text"
-                placeholder="Enter Your Name"
-            />
-        </label>
-        <label for=""
-            >Contact No.:
 
-            <input
-                type="text"
-            id="myInput2"
-
-                placeholder=" Enter Your Mobile No."
-            />
-        </label>
-        <label for=""
-            >Email ID:
-
-            <input
-                type="text"
-            id="myInput3"
-                placeholder=" Enter Your Email ID"
-            />
-        </label>
-        <label class="next">
-            <input
-
-                on:change={onChange}
-                type="radio"
-                value="next"
-            />Next
-        </label>
-    </form>
-</main> -->
+<style>
+  input :disabled {
+    background-color: azure;
+    color: black;
+  }
+</style>
