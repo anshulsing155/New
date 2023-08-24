277dd7a05a8e7358195a69466d5b0293b8826785
Anshul Singh <anshulsing155>
2023-08-24 17:51:00 +0530

    menu update

--- rinn/src/routes/menu.svelte
+++ rinn/src/routes/menu.svelte
@@ -1,6 +1,10 @@
 <script>
     import "../app.css";
-   
+    let isOpen = true;
+
+function toggleMenu() {
+  isOpen = !isOpen;
+}
 </script>
     
 
@@ -39,7 +43,7 @@
             </a>
             <div class="flex items-center lg:order-2">
                 <a
-                    href="#"
+                    href="/login"
                     class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
                     >Sign In</a
                 >
@@ -49,7 +53,7 @@
                     >Get Started</a
                 >
                 <button
-                    data-collapse-toggle="mobile-menu-2"
+                    on:click={toggleMenu}
                     type="button"
                     class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                     aria-controls="mobile-menu-2"
@@ -81,7 +85,7 @@
                 </button>
             </div>
             <div
-                class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+                class="{isOpen ? "hidden" : "block"} justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
                 id="mobile-menu-2"
             >
                 <ul
@@ -92,7 +96,7 @@
                             href="#"
                             class="  py-2 pr-4 pl-3 rounded bg-primary-700 lg:bg-transparent lg:text-gray-700 lg:p-0 dark:text-white">Buy or Sell</a
                         >
-                        <div class="caret m-2"></div>
+                        <!-- <div class="caret m-2"></div> -->
                         <ul
                             class="dropdown-menu absolute hidden bg-white z-50  w-60 text-gray-700 "
                             >
@@ -101,7 +105,7 @@
                                     
                                     <a
                                         class="rounded-t hover:bg-gray-200 py-4 px-6 block whitespace-normal "
-                                        href="#">Apply Now</a
+                                        href="/start/home-loan">Apply Now</a
                                     >
                                 </li>
                                 <li class="hover:bg-gray-200">
@@ -192,7 +196,7 @@
                                 </li>
                             </ul>
                     </li>
-                    <li class="dropdown ">
+                    <li class="dropdown">
                         <a
                             href="#"
                             class=" py-2 pr-4 pl-3 rounded bg-primary-700 lg:bg-transparent lg:text-gray-700 lg:p-0 dark:text-white">LAP</a
@@ -270,9 +274,39 @@
         </div>
     </nav>
 </header>
-
-
-
+<!-- 
+<nav class=" text-green-800">
+    <div class="container mx-auto px-4">
+      <div class="flex items-center justify-between py-3">
+        <div class="flex items-center">
+          <a href="#" class="text-xl font-bold">ऋण.In</a>
+        </div>
+        <div class="hidden md:flex space-x-4">
+          <a href="#" class="hover:text-gray-300"></a>
+          <a href="#" class="hover:text-gray-300">About</a>
+          <a href="#" class="hover:text-gray-300">Services</a>
+          <a href="#" class="hover:text-gray-300">Contact</a>
+        </div>
+        <div class="md:hidden">
+          <button class="focus:outline-none" on:click={toggleMenu}>
+            <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M4 6h16M4 12h16M4 18h16"></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+      {#if isOpen}
+        <div class="md:hidden mt-2 space-y-2">
+          <a href="#" class="block hover:text-gray-300">Home</a>
+          <a href="#" class="block hover:text-gray-300">About</a>
+          <a href="#" class="block hover:text-gray-300">Services</a>
+          <a href="#" class="block hover:text-gray-300">Contact</a>
+        </div>
+      {/if}
+    </div>
+  </nav> -->
+<!-- 
 <style>
     .dropdown:hover .dropdown-menu {
   display: block;
@@ -288,4 +322,39 @@
 .caret-rotate {
     transform: rotate(180deg);
 }
-</style>
+</style> -->
+<!-- 
+<nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <a href="https://flowbite.com/" class="flex items-center">
+        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo">
+        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+    </a>
+    <div class="flex md:order-2">
+        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
+        <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+          <span class="sr-only">Open main menu</span>
+          <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+          </svg>
+      </button>
+    </div>
+    <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+      <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <li>
+          <a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+        </li>
+      </ul>
+    </div>
+    </div>
+  </nav>
+   -->
--- rinn/src/routes/mortgage-rates/purchase/+page.svelte
+++ rinn/src/routes/mortgage-rates/purchase/+page.svelte
@@ -1,35 +1,91 @@
 <script>
+    
     import Menu from "../../menu.svelte";
+    import Footer from "../../footer.svelte";
+    let isOpen = true;
+    function toggleDropdown() {
+        isOpen = !isOpen;
+    }
 </script>
-<Menu />
-<section class="bg-blue-100">
-    <div class="flex justify-around">
-        <h1 class="">Purchase</h1>
-        <p class="">mortgage rates today for 
-            <span class="font-bold underline">110003</span>
-            </p>
-    </div>
-    <div class="flex">
-        <p class="text-2xl m-10">
-        <span>    No commissions.</span><br>
-        ,<span>   Fast pre-approval, with no hard credit pull. </span> 
-        </p>
 
-    </div>
-    <small>
-        The below rates are estimated rates current as of: 11:47 a.m. GMT+5:30 on May 24
-    </small>
-    <div class="m-auto">
-        <p class="m-auto">
-            Looks like we couldn’t find any loans that matched.
-        </p >
-        <p class="m-auto">
-            Try making some adjustments to broaden your options
+<Menu />
+<section class=" bg-green-50">
+    <div class=" container m-auto">
+        <div class="inline-flex ml-14">
+            <button
+                class="inline-flex rounded-md px-3 py-2 text-5xl font-bold text-green-700"
+                id="menu-button"
+                on:click={toggleDropdown}
+            >
+                Purchase
+                <svg
+                    class=" h-5 mt-3 w-5 text-green-800"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                >
+                    <path
+                        fill-rule="evenodd"
+                        d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                        clip-rule="evenodd"
+                    />
+                </svg>
+            </button>
+        </div>
+        {#if !isOpen}
+            <div
+                class="ml-14 absolute z-10 mt-2 w-56 origin-top-center rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                role="menu"
+                aria-orientation="vertical"
+                aria-labelledby="menu-button"
+                tabindex="-1"
+            >
+                <div class="py-1 px-5" role="none">
+                    <a
+                        href="/"
+                        class="text-gray-700 block px-4 py-2 text-lg"
+                        role="menuitem"
+                        tabindex="-1"
+                        id="menu-item-0">Purchase</a
+                    >
+                    <a
+                        href="/refinance"
+                        class="text-gray-700 block px-4 py-2 text-lg"
+                        role="menuitem"
+                        tabindex="-1"
+                        id="menu-item-1">Refinance</a
+                    >
+                    <a
+                        href="lap"
+                        class="text-gray-700 block px-4 py-2 text-lg"
+                        role="menuitem"
+                        tabindex="-1"
+                        id="menu-item-2">LAP</a
+                    >
+                </div>
+            </div>
+        {/if}
+        <p
+            class="ml-14 px-3 md:mr-20 md:inline md:float-right my-4 rounded-md text-2xl text-gray-500 font-semibold"
+            id="menu-button"
+        >
+            Mortgage rates today for <span class="underline text-green-600">122017</span>
         </p>
     </div>
-    <div class="flex justify-around">
-        <h2>Have another rate? <span><a href=""> Let us match it → </a></span></h2>
-        <div class="flex bg-red-800 h-14 w-40"></div>
-        <div class=" flex bg-blue-800 h-14 w-40"></div>
+    <div>
+        <h2 class="mx-20 px-3 mt-5">
+            <div class="text-green-700 font-light text-4xl m-3">No commissions.</div>
+            <div class="text-blue-800 font-thin text-5xl m-3 font-sans">
+                Fast pre-approval, with no hard credit pull.
+            </div>
+        </h2>
+    </div>
+    <div>
+        <p>
+            <small>
+                The below rates are estimated rates current as of: 2:41 p.m. GMT+5:30 on Aug 24
+            </small>
+        </p>
     </div>
 </section>
+<Footer />
