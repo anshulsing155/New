15fc442ce231560868f936930fefc4a9f7757562
mk983793 <142878738+mk983793@users.noreply.github.com>
2023-08-27 23:21:49 -0700

    Add files via upload
    some pages added

---
+++ rinn/src/routes/find-an-agent/+page.svelte
@@ -0,0 +1,55 @@
+<script>
+    import Menu from '../menu.svelte';
+    import Footer from '../footer.svelte'
+   
+</script>
+
+<Menu />
+<section class="bg-white-50 lg:flex">
+    <div class="container w-1/2 mx-24 mt-14  ">
+        <div class="text-green-700 font-semibold ml-8 text-xl">
+            <p>Better Real Estate</p>
+        </div>
+        <div class="font-semibold ml-8 text-5xl mt-3">
+            <h1>Get an agent who can help</h1> 
+            <h1>you make the right offer</h1>
+        </div>
+        <div class="ml-12 mt-8 inline-flex">
+
+            <span class="dot mt-3">
+                <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 m-auto mt-2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                  </svg>
+                  
+            </span>
+            <div class="text-2xl font-semibold ml-4 ">
+                <p>Use data-driven insights to find out exactly how <br> much to offer</p>
+            </div>
+        </div>
+        <div class="bg-green-600 w-36 h-14 rounded ml-8 mt-10">
+            <button class="text-slate-100 text-xl font-semibold ml-8 mt-3">Get Start</button>
+        </div>
+
+    </div>
+    <div class="w-auto">
+        <img src="src/lib/img/welcome-step-hero.png">
+    </div>
+
+
+</section>
+
+
+
+<Footer />
+<style>
+    .dot {
+  height: 40px;
+  width: 40px;
+  background-color: rgb(85, 158, 85);
+  border-radius: 50%;
+  display: inline-block;
+}
+</style>
+
+
+
--- rinn/src/routes/mortgage-rates/purchase/+page.svelte
+++ rinn/src/routes/mortgage-rates/purchase/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-    
     import Menu from "../../menu.svelte";
     import Footer from "../../footer.svelte";
     let isOpen = true;
@@ -31,9 +30,10 @@
                     />
                 </svg>
             </button>
-            {#if !isOpen}
+        </div>
+        {#if !isOpen}
             <div
-                class="absolute z-10 w-2/5 mt-16 origin-top-center rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                class="ml-14 absolute z-10 mt-2 w-56 origin-top-center rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                 role="menu"
                 aria-orientation="vertical"
                 aria-labelledby="menu-button"
@@ -64,18 +64,20 @@
                 </div>
             </div>
         {/if}
-        </div>
-        
         <p
-            class="ml-14 px-3 md:mr-20 md:inline-block md:float-right my-4 rounded-md text-2xl text-gray-500 font-semibold"
+            class="ml-14 px-3 md:mr-20 md:inline md:float-right my-4 rounded-md text-2xl text-gray-500 font-semibold"
             id="menu-button"
         >
-            Mortgage rates today for <span class="underline text-green-600">122017</span>
+            Mortgage rates today for <span class="underline text-green-600"
+                >122017</span
+            >
         </p>
     </div>
     <div>
         <h2 class="mx-20 px-3 mt-5">
-            <div class="text-green-700 font-light text-4xl m-3">No commissions.</div>
+            <div class="text-green-700 font-light text-4xl m-3">
+                No commissions.
+            </div>
             <div class="text-blue-800 font-thin text-5xl m-3 font-sans">
                 Fast pre-approval, with no hard credit pull.
             </div>
@@ -83,10 +85,64 @@
     </div>
     <div>
         <p>
-            <small>
-                The below rates are estimated rates current as of: 2:41 p.m. GMT+5:30 on Aug 24
+            <small class="ml-4">
+                The below rates are estimated rates current as of: 2:41 p.m.
+                GMT+5:30 on Aug 24
             </small>
         </p>
     </div>
+    <div class="grid justify-items-center mt-4">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+        >
+            <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+            />
+        </svg>
+        <p class="font-semibold">
+            Looks like we couldn’t find any loans that matched.
+        </p>
+        <p>Try making some adjustments to broaden your options.</p>
+    </div>
+    <div class="inline-flex ml-24 mt-8">
+        <p class="text-2xl font-normal">Have another rate?</p>
+        <a href="#" class="font-normal text-2xl text-green-600"
+            >Let us match it →</a
+        >
+    </div>
+    <br />
+    <div class="grid justify-items-center lg:grid-cols-2 mt-4">
+        <div>
+    
+           <iframe class= "lg:h-96 lg:w-96" src="https://www.youtube.com/embed/r9jwGansp1E"/>
+           <p class="font-semibold md:w-72 lg:w-96 "><a href="https://www.youtube.com/embed/r9jwGansp1E">The diffrence Between "Fixed rate" and "Adjustable"</a></p>
+        </div>
+        <div>
+            
+            <iframe class= "lg:h-96 lg:w-96" src="https://www.youtube.com/embed/r9jwGansp1E"/>
+            <p class="font-semibold ml-4 md:w-72 lg:w-96"><a href="https://www.youtube.com/embed/r9jwGansp1E">What are the points and credits?</a></p>
+        </div>
+    </div >
+    <div class=" mt-8">
+        <ul class="list-disc text-sm ml-32 mr-16 mt-8">
+            <li>Rates can change several times a day, so we make sure you have the latest. We updated them at 3:08 a.m. PDT on Aug 25. Interest rates and APRs are for informational purposes and do not include all applicable fees. Your actual rates, payments, and costs may differ.</li>
+            <li>Rates and fees are as of time displayed above and are subject to change without notice.</li>
+            <li>The one-time costs shown include points/credits and third-party fees. An escrow deposit, pre-paid interest, and other charges may be required depending on your situation.</li>
+            <li>We don’t yet have your complete financial picture. Your actual rate, payment and costs could be higher. Get an official Loan Estimate before choosing a loan.</li>
+            <li>Loan approval is subject to underwriter review: not everyone who applies will be approved.</li>
+            <li>We also assume: closing costs are paid out of pocket; your debt-to-income ratio is below 35%; you are purchasing or refinancing a single-family home that is your primary residence; you are making a down payment of 20%; and your credit score is 760 or higher.</li>
+            <li>Refinancing may cause your finance charges to be higher over the life of the loan.</li>
+        </ul>
+
+
+    </div>
+   
 </section>
 <Footer />
