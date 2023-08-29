8f393dac77269d3423a7cf131d38df4e87b31bf3
Anshul Singh <anshulsing155>
2023-08-29 19:26:06 +0530

    added

--- rinn/src/lib/database/mongo.js
+++ rinn/src/lib/database/mongo.js
@@ -7,7 +7,7 @@ async function connectToCluster() {
 	try {
 		mongoClient = new MongoClient("mongodb+srv://rinn:rinn@dev-cluster.s2g8r6v.mongodb.net/");
 		console.log('Connecting to MongoDB Atlas cluster...');
-		await mongoClient.connect();
+		await mongoClient.connect(); 	
 		console.log('Successfully connected to MongoDB Atlas!');
 
 		return mongoClient;
