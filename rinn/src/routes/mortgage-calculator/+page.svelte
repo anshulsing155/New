c18bd119edfbe04d539db4ef7e7a4550b8438d66
Anshul Singh <86312890+anshulsing155@users.noreply.github.com>
2023-10-17 17:07:24 +0530

    Update +page.svelte

--- rinn/src/routes/mortgage-calculator/+page.svelte
+++ rinn/src/routes/mortgage-calculator/+page.svelte
@@ -2,7 +2,7 @@
 
 	import Menu from "../menu.svelte";
 	import Footer from "../footer.svelte";
-    import Homeloanemi1_cal from "../components/hloan_emi_cal.svelte"
+    	import Homeloanemi1_cal from "../components/hloan_emi_cal.svelte"
 
 </script>
 <Menu data />
